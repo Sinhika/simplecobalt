7e42cb2edc961a13cc8aa9aba4974b99d55f77f4
Sinhika <sinhika@republicofnewhome.org>
2021-06-20 15:38:28 -0500

    started added skeleton of mod

---
+++ src/main/java/mod/akkamaddi/simplecobalt/ForgeEventSubscriber.java
@@ -0,0 +1,76 @@
+package mod.akkamaddi.simplecobalt;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mod.alexndr.simplecorelib.helpers.ArmorUtils;
+import mod.alexndr.simplecorelib.helpers.LootUtils;
+import mod.alexndr.simpleores.generation.OreGeneration;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SimpleCobalt.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public final class ForgeEventSubscriber
+{
+    private static final Logger LOGGER = LogManager.getLogger(SimpleCobalt.MODID + " Forge Event Subscriber");
+    private static final SimpleCobaltInjectionLookup lootLookupMap = new SimpleCobaltInjectionLookup();
+
+    /**
+     * add mod loot to loot tables. Code heavily based on Botania's LootHandler, which
+     * neatly solves the problem when I couldn't figure it out.
+     */
+    @SubscribeEvent
+    public static void LootLoad(final LootTableLoadEvent event)
+    {
+        if (SimpleCobaltConfig.addModLootToChests)
+        {
+            LootUtils.LootLoadHandler(SimpleCobalt.MODID, event, lootLookupMap);
+        } // end-if
+    } // end LootLoad()
+    
+    /**
+     * Biome loading triggers ore generation.
+     */
+    @SubscribeEvent(priority=EventPriority.HIGH)
+    public static void onBiomeLoading(BiomeLoadingEvent evt)
+    {
+        if (evt.getCategory() != Biome.Category.NETHER && evt.getCategory() != Biome.Category.THEEND)
+        {
+            OreGeneration.generateOverworldOres(evt);
+        }
+   } // end onBiomeLoading()
+    
+     /**
+     * Handle special armor immunities: blue_drift_steel cancels falling damage.
+     * 
+     * @param event
+     */
+    @SubscribeEvent(receiveCanceled = true, priority= EventPriority.HIGHEST)
+    public static void onLivingAttackEvent(LivingAttackEvent event)
+    {
+        // first, is it a player?
+        if (event.getEntityLiving() instanceof PlayerEntity)
+        {
+            PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+            LOGGER.debug("caught LivingAttackEvent");
+
+            // falling anvils and other impact damage, wearing full suit of tungsten carbide?
+            if (event.getSource() == DamageSource.FALL 
+                &&
+                ArmorUtils.isPlayerWearingFullSet(player, SimpleCobaltArmorMaterial.BLUE_DRIFT_STEEL))
+            {
+                // pro-forma cancelable check.
+                if (event.isCancelable()) event.setCanceled(true);
+                LOGGER.debug("Canceled fall damage because of blue drift steel armor set");
+            } // end-if full set of blue drift steel and fall damage
+        } // end-if player
+    } // end onLivingHurtEvent
+
+} // end class
---
+++ src/main/java/mod/akkamaddi/simplecobalt/ModEventSubscriber.java
@@ -0,0 +1,97 @@
+package mod.akkamaddi.simplecobalt;
+
+import javax.annotation.Nonnull;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mod.alexndr.simplecorelib.config.FlagCondition;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@Mod.EventBusSubscriber(modid = SimpleCobalt.MODID,  bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class ModEventSubscriber
+{
+    private static final Logger LOGGER = LogManager.getLogger(SimpleCobalt.MODID + " Mod Event Subscriber");
+    
+    /**
+     * For best inter-mod compatibility, run ore generation in a DeferredWorkQueue, per dieseiben07.
+     * @param event
+     */
+    @SubscribeEvent
+    public static void onCommonSetup(final FMLCommonSetupEvent event)
+    {
+        event.enqueueWork(() -> {
+            OreGeneration.initOverworldFeatures();
+      });
+        LOGGER.debug("Common setup done");
+    } // end onCommonSetup
+
+    
+    @SubscribeEvent
+    public static void onModConfigEvent(final ModConfig.ModConfigEvent event)
+    {
+        final ModConfig config = event.getConfig();
+
+        // Rebake the configs when they change
+        if (config.getSpec() == ConfigHolder.CLIENT_SPEC)
+        {
+            ConfigHelper.bakeClient(config);
+        }
+        else if (config.getSpec() == ConfigHolder.SERVER_SPEC)
+        {
+            ConfigHelper.bakeServer(config);
+        }
+    } // onModConfigEvent
+
+    /**
+     * This method will be called by Forge when it is time for the mod to register its Items.
+     * This method will always be called after the Block registry method.
+     */
+    @SubscribeEvent
+    public static void onRegisterItems(final RegistryEvent.Register<Item> event)
+    {
+        final IForgeRegistry<Item> registry = event.getRegistry();
+
+        // We need to go over the entire registry so that we include any potential Registry Overrides
+        // Automatically register BlockItems for all our Blocks
+        ModBlocks.BLOCKS.getEntries().stream()
+                .map(RegistryObject::get)
+                // You can do extra filtering here if you don't want some blocks to have an BlockItem automatically registered for them
+                // .filter(block -> needsItemBlock(block))
+                // Register the BlockItem for the block
+                .forEach(block -> {
+                    // Make the properties, and make it so that the item will be on our ItemGroup (CreativeTab)
+                    final Item.Properties properties = 
+                            new Item.Properties().tab(ModTabGroups.MOD_ITEM_GROUP);
+                    // Create the new BlockItem with the block and it's properties
+                    final BlockItem blockItem = new BlockItem(block, properties);
+                    // Set the new BlockItem's registry name to the block's registry name
+                    blockItem.setRegistryName(block.getRegistryName());
+                    // Register the BlockItem
+                    registry.register(blockItem);
+                });
+        LOGGER.debug("Registered BlockItems");
+    }  // end onRegisterItems()
+
+    
+    @SubscribeEvent
+    public static void onRegisterRecipeSerializers(
+            @Nonnull final RegistryEvent.Register<IRecipeSerializer<?>> event)
+    {
+        CraftingHelper.register(new FlagCondition.Serializer(SimpleTungstenConfig.INSTANCE, 
+                new ResourceLocation(SimpleCobalt.MODID, "flag")));
+    } // end registerRecipeSerializers
+
+
+} // end class
