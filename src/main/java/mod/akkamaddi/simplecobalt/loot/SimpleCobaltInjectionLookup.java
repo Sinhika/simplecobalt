f34b57700f012ebf2e410e7d0939b9df6452dd41
Sinhika <sinhika@republicofnewhome.org>
2021-07-03 09:51:23 -0500

    added skeleton of loot injection lookup table

---
+++ src/main/java/mod/akkamaddi/simplecobalt/loot/SimpleCobaltInjectionLookup.java
@@ -0,0 +1,13 @@
+package mod.akkamaddi.simplecobalt.loot;
+
+import mod.alexndr.simplecorelib.helpers.InjectionTableLookup;
+
+public class SimpleCobaltInjectionLookup extends InjectionTableLookup
+{
+
+    public SimpleCobaltInjectionLookup()
+    {
+        super();
+    }
+
+} // end class
