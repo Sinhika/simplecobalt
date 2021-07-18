cb82264f96ab05bae88348d3fb746563e640ae1b
Sinhika <sinhika@republicofnewhome.org>
2021-07-17 19:03:06 -0500

    filled in ModBlockTags and ModItemTags

--- src/main/java/mod/akkamaddi/simplecobalt/datagen/ModBlockTags.java
+++ src/main/java/mod/akkamaddi/simplecobalt/datagen/ModBlockTags.java
@@ -1,8 +1,11 @@
 package mod.akkamaddi.simplecobalt.datagen;
 
+import mod.akkamaddi.simplecobalt.init.ModBlocks;
 import mod.akkamaddi.simplecobalt.SimpleCobalt;
+import mod.alexndr.simplecorelib.helpers.TagUtils;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class ModBlockTags extends BlockTagsProvider
@@ -13,4 +16,43 @@ public class ModBlockTags extends BlockTagsProvider
         super(generatorIn, SimpleCobalt.MODID, existingFileHelper);
     }
 
+    @Override
+    protected void addTags()
+    {
+        registerStorageBlockTags();
+        registerBeaconBlockTags();
+    } // end registerTags()
+ 
+    /**
+     * Create standard forge tags for storage blocks.
+     */
+    private void registerStorageBlockTags()
+    {
+        this.tag(TagUtils.forgeBlockTag("storage_blocks"))
+            .addTag(TagUtils.forgeBlockTag("storage_blocks/cobalt_block"))
+            .addTag(TagUtils.forgeBlockTag("storage_blocks/blue_drift_steel_block"))
+            .addTag(TagUtils.forgeBlockTag("storage_blocks/blue_celadon_block"))
+            .addTag(TagUtils.forgeBlockTag("storage_blocks/green_celadon_block"));
+        
+        this.tag(TagUtils.forgeBlockTag("storage_blocks/cobalt_block"))
+            .add(ModBlocks.cobalt_block.get());
+        this.tag(TagUtils.forgeBlockTag("storage_blocks/blue_drift_steel_block"))
+            .add(ModBlocks.blue_drift_steel_block.get());
+        this.tag(TagUtils.forgeBlockTag("storage_blocks/blue_celadon_block"))
+            .add(ModBlocks.blue_celadon_block.get());
+        this.tag(TagUtils.forgeBlockTag("storage_blocks/green_celadon_block"))
+            .add(ModBlocks.green_celadon_block.get());
+    } // end registerStorageBlockTags
+    
+    /**
+     * Identify metal blocks as beacon block bases.
+     */
+    private void registerBeaconBlockTags()
+    {
+        this.tag(BlockTags.BEACON_BASE_BLOCKS)
+            .add(ModBlocks.cobalt_block.get())
+            .add(ModBlocks.blue_drift_steel_block.get())
+            .add(ModBlocks.blue_celadon_block.get())
+            .add(ModBlocks.green_celadon_block.get());
+    }
 } // end class
--- src/main/java/mod/akkamaddi/simplecobalt/datagen/ModItemTags.java
+++ src/main/java/mod/akkamaddi/simplecobalt/datagen/ModItemTags.java
@@ -1,9 +1,11 @@
 package mod.akkamaddi.simplecobalt.datagen;
 
 import mod.akkamaddi.simplecobalt.SimpleCobalt;
+import mod.alexndr.simplecorelib.helpers.TagUtils;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import mod.akkamaddi.simplecobalt.init.ModItems;
 
 public class ModItemTags extends ItemTagsProvider
 {
@@ -13,5 +15,40 @@ public class ModItemTags extends ItemTagsProvider
         super(dataGenerator, new ModBlockTags(dataGenerator, existingFileHelper), SimpleCobalt.MODID, 
                 existingFileHelper);
     }
+
+    @Override
+    protected void addTags()
+    {
+        registerNuggetTags();
+        registerIngotTags();
+    }
+
+    private void registerNuggetTags()
+    {
+        this.tag(TagUtils.forgeTag("nuggets"))
+            .addTag(TagUtils.forgeTag("nuggets/cobalt"))
+            .addTag(TagUtils.forgeTag("nuggets/blue_drift_steel"))
+            .addTag(TagUtils.forgeTag("nuggets/blue_celadon"))
+            .addTag(TagUtils.forgeTag("nuggets/green_celadon"));
+    
+        this.tag(TagUtils.forgeTag("nuggets/cobalt")).add(ModItems.cobalt_nugget.get());
+        this.tag(TagUtils.forgeTag("nuggets/blue_drift_steel")).add(ModItems.blue_drift_steel_nugget.get());
+        this.tag(TagUtils.forgeTag("nuggets/blue_celadon")).add(ModItems.blue_celadon_nugget.get());
+        this.tag(TagUtils.forgeTag("nuggets/green_celadon")).add(ModItems.green_celadon_nugget.get());
+    } // end registerNuggetTags()
+    
+    private void registerIngotTags()
+    {
+        this.tag(TagUtils.forgeTag("ingots"))
+            .addTag(TagUtils.forgeTag("ingots/cobalt"))
+            .addTag(TagUtils.forgeTag("ingots/blue_drift_steel"))
+            .addTag(TagUtils.forgeTag("ingots/blue_celadon"))
+            .addTag(TagUtils.forgeTag("ingots/green_celadon"));
+        
+        this.tag(TagUtils.forgeTag("ingots/cobalt")).add(ModItems.cobalt_ingot.get());
+        this.tag(TagUtils.forgeTag("ingots/blue_drift_steel")).add(ModItems.blue_drift_steel_ingot.get());
+        this.tag(TagUtils.forgeTag("ingots/blue_celadon")).add(ModItems.blue_celadon_ingot.get());
+        this.tag(TagUtils.forgeTag("ingots/green_celadon")).add(ModItems.green_celadon_ingot.get());
+    } // end registerIngotTags()
 
 } // end class
